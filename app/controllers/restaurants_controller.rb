918a296f542bf48b4b603d89985b771a399d42f0
eunkenlow <eunkenlow@gmail.com>
2016-11-14 17:37:11 +0000

    edit code

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,10 +3,29 @@ class RestaurantsController < ApplicationController
 
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def create
-    
-    @restaurant = Restaurant.new(params[:restaurant])
+    @restaurant = Restaurant.new(restaurant_params)
+
     @restaurant.save
     redirect_to @restaurant
   end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description)
+  end
+
 end

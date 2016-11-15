a86acfa01517f24830817eac7700b77d3a6b21b2
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-15 14:17:05 +0000

    reviews working

--- spec/features/review_create_spec.rb
+++ spec/features/review_create_spec.rb
@@ -1,13 +1,15 @@
   feature  'reviews management' do
-    scenario 'create a restaurant' do
-      Restaurant.new(name: 'Oi Hanoi', description: 'Now open tonight!')
+    scenario 'create a review' do
+      Restaurant.new(name: 'Oi Hanoi', description: 'Now open in London tonight!')
       visit '/restaurants'
       click_link 'Show'
-      click_link 'Create a new review'
+      fill_in 'Email', with: 'laurent@makers.com'
       fill_in 'Text', with: 'Best restaurant in town!'
       fill_in 'Rating', with: '5'
-      click_button 'Save review'
+      click_button 'Create review'
 
+      expect(page).to have_content('laurent@makers.com')
       expect(page).to have_content('Best restaurant in town!')
+      expect(page).to have_content('5')
     end
   end

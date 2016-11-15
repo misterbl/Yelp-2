9c2522ac6734ae7663a90f1988ad2a7b4f1d634e
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-15 10:03:38 +0000

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114161005) do
+ActiveRecord::Schema.define(version: 20161115095212) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.text     "text"
+    t.integer  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

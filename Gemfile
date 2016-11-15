292ca7be2c3eb67b9ce64f37b017e10312e50073
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-15 15:44:44 +0000

    password confirmation and encryption

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ group :test do
 end
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.11'
 
 # Use unicorn as the app server
 # gem 'unicorn'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new
   end
 
   def create
@@ -18,6 +17,6 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end

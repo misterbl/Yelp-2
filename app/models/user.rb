fdc389118e221e5c85e4a44d2eef2bd4403669e6
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-11-16 13:00:37 +0000

    devise installation done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :restaurants
   has_many :reviews
  validates :username,  presence: true, length: { maximum: 50 }

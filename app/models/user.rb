fd73957776eb01c079c54e5a3660aacb5682f6aa
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-11-17 13:06:25 +0000

    facebook login procedure added, broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
   has_many :restaurants
   has_many :reviews
- # validates :username,  presence: true, length: { maximum: 50 }
-#  validates :email, presence: true, length: { maximum: 255 },
-#                    uniqueness: { case_sensitive: false }
-# # has_secure_password
-#  validates :password, presence: true, length: { minimum: 6 }
+
+def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.username = auth.info.name   # assuming the user model has a name
+    user.avatar = auth.info.image # assuming the user model has an image
+  end
+end
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -36,6 +36,11 @@ Devise.setup do |config|
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [:email]
 
+  # facebook authentication
+  config.omniauth :facebook, "376687579339706", "1aa292a06196f8b3ed1ae946174aa4c5",
+                  callback_url: "http://localhost:3000/users/auth/facebook/callback"
+
+
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
   # find_for_authentication method and considered in your model lookup. For instance,

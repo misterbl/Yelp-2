b5be5cc27fb6c62ed847dd499b4fcc8067cca50f
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-18 14:07:20 +0000

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,8 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
 
   }
-  devise_scope :users do
+
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
 
   resources :restaurants
 
